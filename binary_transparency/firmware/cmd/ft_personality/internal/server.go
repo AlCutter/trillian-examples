e11756ff7b5cae85f16f0b8bef342173f99cb1fe
Al Cutter <al@google.com>
2020-08-13 20:11:45 +0100

    Add parsing of metadata in add_firmware handler

--- binary_transparency/firmware/cmd/ft_personality/internal/server.go
+++ binary_transparency/firmware/cmd/ft_personality/internal/server.go
@@ -2,18 +2,96 @@
 package internal
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"mime"
+	"mime/multipart"
 	"net/http"
+	"strings"
+
+	"github.com/golang/glog"
+	"github.com/google/trillian-examples/binary_transparency/firmware/api"
 )
 
 // Server is the core state & handler implementation of the FT personality.
 type Server struct {
 }
 
+// addFirmware handles requests to log new firmware images.
+// It expects a mime/multipart POST consisting of FirmwareMetadata json,
+// followed by a signature over those bytes.
+// TODO: store the actual firmware image in a CAS too.
+//
+// curl -i -X POST -H "Content-Type: multipart/mixed" -F 'json=@testdata/firmware_metadata.json;type=application/json' -F 'sig=@testdata/firmware_metadata.json.sig' localhost:8000/ft/v0/add_firmware
 func (s *Server) addFirmware(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotImplemented)
+
+	h := r.Header["Content-Type"]
+	if len(h) == 0 {
+		http.Error(w, "no content-type header", http.StatusBadRequest)
+		return
+	}
+
+	mediaType, mediaParams, err := mime.ParseMediaType(h[0])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if !strings.HasPrefix(mediaType, "multipart/") {
+		http.Error(w, "expecting mime multipart body", http.StatusBadRequest)
+		return
+	}
+	boundary := mediaParams["boundary"]
+	if len(boundary) == 0 {
+		http.Error(w, "invalid mime multipart header - no boundary specified", http.StatusBadRequest)
+		return
+	}
+	mr := multipart.NewReader(r.Body, boundary)
+
+	// Get raw JSON
+	p, err := mr.NextPart() // JSON body section
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	rawJSON, err := ioutil.ReadAll(p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	// Get signature
+	p, err = mr.NextPart() // Sig section
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	rawSig, err := ioutil.ReadAll(p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// "Verify" the signature:
+	if sigStr := string(rawSig); sigStr != "LOL!" {
+		http.Error(w, fmt.Sprintf("invalid LOL! sig %q", sigStr), http.StatusBadRequest)
+		return
+	}
+
+	// Parse the JSON:
+	var meta api.FirmwareMetadata
+	if err := json.Unmarshal(rawJSON, &meta); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	glog.V(1).Infof("Got firmware %+v", meta)
+
+	meta.Raw = rawJSON
+	meta.Signature = rawSig
+
 }
 
 // RegisterHandlers registers HTTP handlers for firmware transparency endpoints.
 func (s *Server) RegisterHandlers() {
-	http.HandleFunstec("/ft/v0/add_firmware", s.addFirmware)
+	http.HandleFunc("/ft/v0/add_firmware", s.addFirmware)
 }
